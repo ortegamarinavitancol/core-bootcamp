94534c41e5da0c4518f429efecc04e01edb14ac9
ortegamarinavitancol <marina.ortega@adamson.edu.ph>
2025-02-28 10:25:54 +0800

    first commit

--- 01-solidity/contracts/03_solidity_functions.sol
+++ 01-solidity/contracts/03_solidity_functions.sol
@@ -6,14 +6,32 @@ contract SolidityFunctions {
     uint256 balance = 0;
 
     constructor(uint256 initialBalance) {
-        // balance = initialBalance;
+        balance = initialBalance;
     }
 
     function addBalance(uint256 toAddBalance) public {
-        // balance += toAddBalance;
+        balance += toAddBalance;
     }
 
     function getBalance() public view returns (uint256) {
-        // return balance;
+        return balance;
+    }
+
+    function calculator(uint mode, uint256 amount) public {
+        if (mode == 1) {
+            balance += amount;
+        } 
+
+        else if (mode == 2) {
+            balance -= amount;
+        } 
+
+        else if (mode ==3){
+            balance *= amount;
+        }
+
+        else if (mode ==4){
+            balance /= amount;
+        }
     }
 }
